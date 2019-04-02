f57083b05a2a678c2ee5663f3c6e6c00e2fc6b15
Garry Liu <garry1994916@gmail.com>
2019-04-02 11:16:32 -0400

    add Info struct and Info buffer fields. add header print

--- src/printer.cc
+++ src/printer.cc
@@ -1,12 +1,43 @@
 #include "printer.h"
+
 #include <iostream>  // access: cin, cout, cerr, endl
+using namespace std;
 
 Printer::Printer( unsigned int numStudents,
                   unsigned int numVendingMachines,
                   unsigned int numCouriers )
     : numStudents( numStudents ),
       numVendingMachines( numVendingMachines ),
-      numCouriers( numCouriers ) {}
+      numCouriers( numCouriers ),
+      students( numStudents ),
+      machines( numVendingMachines ),
+      couriers( numCouriers ) {
+    cout << "Parent\tGropoff\tWATOff\tNames\tTruck\tPlant\t";
+    for ( unsigned int i = 0; i < numStudents; ++i ) {
+        cout << "Stud" << i << "\t";
+    }
+    for ( unsigned int i = 0; i < numVendingMachines; ++i ) {
+        cout << "Mach" << i << "\t";
+    }
+    for ( unsigned int i = 0; i < numCouriers; ++i ) {
+        if ( i != 0 ) {
+            cout << '\t';
+        }
+        cout << "Cour" << i;
+    }
+    cout << '\n';
+
+    unsigned int total_cols
+        = 6 + numStudents + numVendingMachines + numCouriers;
+
+    for ( unsigned int i = 0; i < total_cols; ++i ) {
+        if ( i != 0 ) {
+            cout << '\t';
+        }
+        cout << "*******";
+    }
+    cout << '\n';
+}
 
 void
 Printer::print( Kind kind, char state ) {}
--- src/printer.h
+++ src/printer.h
@@ -1,11 +1,31 @@
 #ifndef __PRINTER_H__
 #define __PRINTER_H__
 
+#include <vector>
+
 _Monitor Printer {
    private:
+    struct Info {
+        bool dirty = false;
+        char state;
+        int  val1;
+        int  val2;
+    };
+
     unsigned int numStudents;
     unsigned int numVendingMachines;
     unsigned int numCouriers;
+
+    // Info buffer
+    Info              parent;
+    Info              groupoff;
+    Info              watoff;
+    Info              names;
+    Info              truck;
+    Info              plant;
+    std::vector<Info> students;
+    std::vector<Info> machines;
+    std::vector<Info> couriers;
 
    public:
     enum Kind {
@@ -19,6 +39,11 @@ _Monitor Printer {
         Vending,
         Courier
     };
+    /**
+     * @brief Construct for Printer. Will initialize vector<Info>
+     *        buffers for students, vending machines and couriers.
+     *        It will also print out output header.
+     */
     Printer( unsigned int numStudents,
              unsigned int numVendingMachines,
              unsigned int numCouriers );
