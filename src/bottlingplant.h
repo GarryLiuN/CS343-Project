78da5320779ceaa33ac6a89b875dc4a6c630477f
xilingwu <wuxiling@outlook.com>
2019-04-04 14:04:26 -0400

    BP and Truck Doc

--- src/bottlingplant.h
+++ src/bottlingplant.h
@@ -4,6 +4,10 @@
 #include "nameserver.h"
 #include "printer.h"
 
+/**
+ *@brief class BottlingPlant will manage soda production and load soda to the
+ *truck
+ */
 _Task BottlingPlant {
    private:
     // references
@@ -15,18 +19,26 @@ _Task BottlingPlant {
     unsigned int maxStockPerFlavour;
     unsigned int timeBetweenShipments;
     unsigned int currentStock[NUMOFFLAVORS] = {0};  // stock for each flavor
-    bool         isDestructed               = false;
+    // isDestructed will be set to true after BP's destructor is called and next
+    // getShipment will throw event ShutDown
+    bool isDestructed = false;
 
     void main();
 
    public:
-    _Event Shutdown{};  // shutdown plant
+    // shutdown event will be thrown at truck to start its termination
+    _Event Shutdown{};
     BottlingPlant( Printer & prt,
                    NameServer & nameServer,
                    unsigned int numVendingMachines,
                    unsigned int maxShippedPerFlavour,
                    unsigned int maxStockPerFlavour,
                    unsigned int timeBetweenShipments );
+    /**
+     * @brief getShipment will be called by truck and cargo is printer of the
+     * truck's cargo bottling plant will fill the truck's cargo in the function
+     * call.
+     */
     void getShipment( unsigned int cargo[] );
 };
 
