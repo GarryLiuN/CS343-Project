05b7af27c4b618d0a47d57c80cdd64951edf3646
xilingwu <wuxiling@outlook.com>
2019-04-04 14:22:22 -0400

    NS Doc
    Remove duplicate ns finish print message

--- src/nameserver.h
+++ src/nameserver.h
@@ -11,12 +11,22 @@ _Task NameServer {
     // reference
     Printer& prt;
     // attributes
-    unsigned int                 numVendingMachines;
-    unsigned int                 numStudents;
-    std::vector<int>             studentIndex;
-    std::vector<VendingMachine*> machineList;
-    unsigned int                 lastMachineIndex = -1;
+    unsigned int numVendingMachines;
+    unsigned int numStudents;
+    /** @brief studentIndex vector is used to store the last VM index each
+     * student accessed, which will help to make sure that each student has a
+     * chance to
+     */
+    visit every machine std::vector<int> studentIndex;
 
+    std::vector<VendingMachine*> machineList;
+
+    /** @brief lastMachineIndex is used to store the last vm ID that assigned to
+     * student who first time called getMachine
+     */
+    unsigned int lastMachineIndex = -1;
+
+    // local copy for machine that call VMregister
     VendingMachine* newMachine;
 
     void main();
@@ -25,8 +35,22 @@ _Task NameServer {
     NameServer( Printer & prt,
                 unsigned int numVendingMachines,
                 unsigned int numStudents );
-    void             VMregister( VendingMachine * vendingmachine );
-    VendingMachine*  getMachine( unsigned int id );
+    /**
+     * @brief VMregister will be called by a VM and the nameserver will save the
+     * pointer to that machine into a vector
+     */
+    void VMregister( VendingMachine * vendingmachine );
+
+    /**
+     * @brief GetMachine will be called by a student, the id will be used to
+     * select next VM in a round-robin fashion
+     */
+    VendingMachine* getMachine( unsigned int id );
+
+    /**
+     * @brief getMachineList will be called by truck and return the full list of
+     * all VMs
+     */
     VendingMachine** getMachineList();
 };
 
